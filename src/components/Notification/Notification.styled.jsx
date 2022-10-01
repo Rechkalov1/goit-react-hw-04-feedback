2a9689213182d5261f6a1df1736b59401a1c0de3
EnVy <envy7321@gmail.com>
2022-10-01 12:33:25 +0300

    save

---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const  NotificTitle =styled.p`
+display: block;
+text-align:center;
+color: #337AB7;
+font-size:24px;
+`;
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StatisticsThumb =styled.div`
+ justify-content: center;
+ display: flex;
+`;
+export const StatisticsItem = styled.ul`
+display: flex;
+`;
+export const  StatisticsList = styled.li`margin-right:15px;
+ border: 2px solid #BFE2FF;
+ background: #337AB7;
+ font-family: 'Roboto Condensed', Тahoma, sans-serif;
+ color: #FFF;
+ padding: 12px 30px;
+ border-radius: 10px;
+`;
