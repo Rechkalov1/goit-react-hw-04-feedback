8a772cfb57f8eea726ed8aaa36a5d437ad4f255a
EnVy <envy7321@gmail.com>
2022-10-01 13:37:16 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,63 @@
-import { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from 'components/Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
-  countPositiveFeedbackPercentage() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
+import { useState } from 'react';
 
-    return Math.round((good / total) * 100);
-  }
-  IncrementStatistics = propertyName => {
-    this.setState(prevState => {
-      const value = prevState[propertyName];
-      return {
-        [propertyName]: value + 1,
-      };
-    });
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const goodPercent = this.countPositiveFeedbackPercentage();
-    const neutralPercent = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions IncrementStatistics={this.IncrementStatistics} />
-        </Section>
-        <Section title="Statistics">
-          {' '}
-          {this.countTotalFeedback() === 0 ? (
-            <Notification />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              goodPercent={goodPercent}
-              neutralPercent={neutralPercent}
-            />
-          )}
-        </Section>
-      </>
-    );
-  }
+  const countPositiveFeedbackPercentage = good => {
+    const total = countTotalFeedback();
+    if (!total) {
+      return 0;
+    }
+    return Math.round((good / total) * 100);
+  };
+
+  const IncrementStatistics = btnName => {
+    switch (btnName) {
+      case 'good':
+        return setGood(prev => prev + 1);
+
+      case 'neutral':
+        return setNeutral(prev => prev + 1);
+
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const total = countTotalFeedback();
+  const goodPercent = countPositiveFeedbackPercentage(good);
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions IncrementStatistics={IncrementStatistics} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            goodPercent={goodPercent}
+            neutralPercent={countPositiveFeedbackPercentage}
+          />
+        )}
+      </Section>
+    </>
+  );
 }
